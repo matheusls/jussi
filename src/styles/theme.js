2dfde05b67680bdd28e6ddd8528ed4916834b20a
Matheus Luiz da Silva <lsmatheus94@gmail.com>
2021-05-08 18:28:17 -0300

    chore: add theme colors and font sizes

--- src/styles/theme.js
+++ src/styles/theme.js
@@ -1,7 +1,17 @@
 const theme = {
   colors: {
     black: '#000',
+    grey: '#f2f2f2',
+    greyDark: '#5b5b5b',
+    green: '#03ffa5',
+    pink: '#ee0e7d',
     white: '#fff',
+  },
+  fontSizes: {
+    small: 0.75,
+    paragraph: 1,
+    large: 2,
+    xlarge: 4,
   },
 };
 
