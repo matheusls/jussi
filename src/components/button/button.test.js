6402d0badec6cd26405cfc5ed52682644b2778a9
Matheus Luiz da Silva <lsmatheus94@gmail.com>
2021-05-12 01:23:07 -0300

    test: create Button component

---
+++ src/components/button/button.test.js
@@ -0,0 +1,37 @@
+import { theme } from 'styles';
+import { render, screen } from 'tests/utils';
+
+import Button from './button';
+import { buttonVariations } from './get-button-variation';
+
+const buttonVariationNames = Object.keys(buttonVariations);
+
+describe('<Button />', () => {
+  it('should render Button component correctly', () => {
+    render(<Button>button component</Button>);
+
+    expect(screen.getByText(/button component/i)).toBeInTheDocument();
+  });
+
+  it.each(buttonVariationNames)(
+    '%# should render Button with variation %s',
+    (variation) => {
+      const { backgroundColor, borderColor, color } = buttonVariations[
+        variation
+      ];
+
+      const { colors } = theme;
+
+      render(<Button variation={variation}>button component</Button>);
+
+      const button = screen.getByText(/button component/i);
+
+      expect(button).toHaveStyleRule(
+        'background-color',
+        colors[backgroundColor] ?? 'transparent',
+      );
+      expect(button).toHaveStyleRule('border-color', colors[borderColor]);
+      expect(button).toHaveStyleRule('color', colors[color]);
+    },
+  );
+});
