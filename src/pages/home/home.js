78318034cf896d9102cb56b145ea7e05ce836905
Matheus Luiz da Silva <lsmatheus94@gmail.com>
2021-05-12 21:21:01 -0300

    feat: create Home page

--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -1,3 +1,27 @@
-const Home = () => <h1>React App</h1>;
+import {
+  Footer,
+  HeaderHero,
+  Navbar,
+  SectionAbout,
+  SectionContact,
+  SectionNewsletter,
+  SectionSolutions,
+  SectionStores,
+} from 'components';
+
+const Home = () => (
+  <>
+    <Navbar />
+    <main>
+      <HeaderHero />
+      <SectionStores />
+      <SectionSolutions />
+      <SectionAbout />
+      <SectionContact />
+      <SectionNewsletter />
+    </main>
+    <Footer />
+  </>
+);
 
 export default Home;
