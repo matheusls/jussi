c50a597a0cf5064fec90a7a95ba156814342513b
Matheus Luiz da Silva <lsmatheus94@gmail.com>
2021-05-13 22:06:38 -0300

    fix: console error due to FormInline props

--- src/components/form-inline/form-inline.js
+++ src/components/form-inline/form-inline.js
@@ -1,12 +1,9 @@
-import { bool, func, node, shape, string } from 'prop-types';
+import { shape } from 'prop-types';
 
 import {
   FormInlineStyled,
   FormInlineButtonStyled,
   FormInlineInputStyled,
-  formInlineProps,
-  formInlineInputProps,
-  formInlineButtonProps,
 } from './form-inline.styled';
 
 const FormInline = ({ buttonProps, formProps, inputProps }) => (
@@ -17,26 +14,9 @@ const FormInline = ({ buttonProps, formProps, inputProps }) => (
 );
 
 FormInline.propTypes = {
-  formProps: shape({
-    ...formInlineProps,
-    onSubmit: func.isRequired,
-  }),
-  buttonProps: shape({
-    ...formInlineButtonProps,
-    children: node.isRequired,
-    disabled: bool.isRequired,
-    type: string,
-    onClick: func,
-  }),
-  inputProps: shape({
-    ...formInlineInputProps,
-    id: string.isRequired,
-    name: string.isRequired,
-    placeholder: string.isRequired,
-    type: string.isRequired,
-    value: string,
-    onChange: func,
-  }),
+  formProps: shape({}),
+  buttonProps: shape({}),
+  inputProps: shape({}),
 };
 
 export default FormInline;
