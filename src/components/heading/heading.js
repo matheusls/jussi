b3e3a2e50fa52283299726282f386219e3e26931
Matheus Luiz da Silva <lsmatheus94@gmail.com>
2021-05-13 21:33:24 -0300

    feat: add openQuote prop to heading

--- src/components/heading/heading.js
+++ src/components/heading/heading.js
@@ -1,6 +1,7 @@
-import { oneOf } from 'prop-types';
+import { themeGet } from '@styled-system/theme-get';
+import { oneOf, shape, string } from 'prop-types';
 import { color, compose, layout, space, typography } from 'styled-system';
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 
 import { colorAliases, fontSizeAliases } from 'styles';
 import { textTransformOptions } from 'utils';
@@ -16,6 +17,32 @@ const Heading = styled.h1.attrs(({ level }) => ({
   margin: 0;
   text-transform: ${({ textTransform }) => textTransform};
 
+  ${({ openQuote }) =>
+    openQuote &&
+    css`
+      position: relative;
+
+      &::before {
+        bottom: ${openQuote.bottom};
+        content: open-quote;
+        color: ${themeGet(`colors.${openQuote.color}`)};
+        display: inline-block;
+        font-size: ${openQuote.size};
+        font-weight: 300;
+        left: ${openQuote.left};
+        line-height: 0.1rem;
+        margin-right: 0.25rem;
+        position: ${openQuote.position};
+        right: ${openQuote.right};
+        top: ${openQuote.top};
+        vertical-align: bottom;
+      }
+      &::after {
+        visibility: hidden;
+        content: close-quote;
+      }
+    `}
+
   ${compose(color, layout, space, typography)};
 `;
 
@@ -25,6 +52,7 @@ Heading.defaultProps = {
   fontStyle: 'normal',
   fontWeight: 'bold',
   level: 1,
+  openQuote: null,
   textTransform: 'uppercase',
 };
 
@@ -32,6 +60,15 @@ Heading.propTypes = {
   color: oneOf(colorAliases),
   fontSize: oneOf(fontSizeAliases),
   level: oneOf(headingLevels),
+  openQuote: shape({
+    bottom: string.isRequired,
+    color: string.isRequired,
+    left: string.isRequired,
+    position: string.isRequired,
+    right: string.isRequired,
+    size: string.isRequired,
+    top: string.isRequired,
+  }),
   textTransform: oneOf(textTransformOptions),
 };
 
