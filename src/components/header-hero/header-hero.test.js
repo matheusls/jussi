3c860565d72822922b983621b97c05dd922a9818
Matheus Luiz da Silva <lsmatheus94@gmail.com>
2021-05-13 22:01:44 -0300

    test: add openQuote and HeaderCard to HeaderHero component

--- src/components/header-hero/header-hero.test.js
+++ src/components/header-hero/header-hero.test.js
@@ -17,5 +17,13 @@ describe('<HeaderHero />', () => {
     expect(
       screen.getByRole('link', { name: /veja nossas soluções/i }),
     ).toHaveAttribute('href', '#');
+    expect(screen.getByAltText(/foto de uma geladeira/i)).toBeInTheDocument();
+    expect(screen.getByText(/mais detalhes/i)).toBeInTheDocument();
+    expect(screen.getByAltText(/foto de uma batedeira/i)).toBeInTheDocument();
+    expect(screen.getByText(/comprar em 12x/i)).toBeInTheDocument();
+    expect(
+      screen.getByAltText(/foto de uma garrafa de whisky/i),
+    ).toBeInTheDocument();
+    expect(screen.getByText(/adicionar à sacola/i)).toBeInTheDocument();
   });
 });
